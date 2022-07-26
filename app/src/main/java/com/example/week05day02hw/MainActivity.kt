13b9d0f12e9172373a6c611e7776feabbda4306e
feras alnowiser <ferasdev@gmail.com>
2022-07-26 20:04:32 +0300

    connect the views four buttons and one layout

--- app/src/main/java/com/example/week05day02hw/MainActivity.kt
+++ app/src/main/java/com/example/week05day02hw/MainActivity.kt
@@ -10,12 +10,28 @@ import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
+    private var constraintLayout:ConstraintLayout? = null
+    private var buttonNull:Button? = null
+    private var buttonIndex:Button? = null
+    private var buttonToast:Button? = null
+    private var buttonSnack:Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        connectViews()
 
     }
+
+    private fun connectViews() {
+        constraintLayout = findViewById(R.id.container)
+        buttonNull = findViewById(R.id.btn_null)
+        buttonIndex = findViewById(R.id.btn_index)
+        buttonToast = findViewById(R.id.btn_toast)
+        buttonSnack = findViewById(R.id.btn_snack)
+    }
+
 
 
 }
